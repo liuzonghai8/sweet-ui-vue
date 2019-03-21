9b7023861b79b986dc935544c2915c6ec44ef682
LZH147258 <369613662@qq.com>
2019-03-21 13:32:41 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import './plugins/axios'
 
+
+//Plugins
+import './plugins'
+
+//
+import App from './App.vue'
+import router from '@/router'
+import store from '@/store'
+
+// Sync store with router
+sync(store, router)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
